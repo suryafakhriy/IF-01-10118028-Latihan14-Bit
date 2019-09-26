40241ab41e792fb2e4e7e2a8805ef83af0fc582c
suryafakhriy <55834014+suryafakhriy@users.noreply.github.com>
2019-09-26 21:49:41 +0700

    Add files via upload

---
+++ IF-01-10118028-Latihan14-Bit/src/pkgif/pkg01/pkg10118028/latihan14/bit/IF0110118028Latihan14Bit.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgif.pkg01.pkg10118028.latihan14.bit;
+
+/**
+ *
+ * @author User
+ * NAMA      : Surya Fakhriy Hasbi
+   KELAS     : IF-01 2018
+   NIM       : 10118028
+   DESKRIPSI : Bit
+ */
+public class IF0110118028Latihan14Bit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 60; /* 60 = 0011 1100*/
+        int b = 13; /* 13 = 0000 1101*/
+        int c = 0;
+        c = a & b;
+        /* 12 = 0000 1100 */
+        System.out.println("a & b = " + c);
+        c = a | b;
+        /* 61 = 0011 1101 */
+        System.out.println("a | b = " + c);
+        c = a ^ b;
+        /* 49 = 0011 0001 */
+        System.out.println("a ^ b = " + c);
+        c = ~a;
+        /* -61 = 1100 0011*/
+        System.out.println("~a = " + c);
+        c = a << 2;
+        /* 240 = 1111 0000*/
+        System.out.println("a << 2 = " + c);
+        c = a >> 2;
+        /* 15 = 0000 1111*/
+        System.out.println("a >> 2 = " + c);
+    } 
+    
+}
